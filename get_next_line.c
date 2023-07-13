d51a790c1eff0a9c697eafbd359f754319dbbd40
u4s2e0r <prioux.mateo@outlook.com>
2023-07-13 12:10:12 +0200

    first try completed, buffer_size max now...

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,35 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 17:12:16 by pmateo            #+#    #+#             */
-/*   Updated: 2023/07/04 17:12:17 by pmateo           ###   ########.fr       */
+/*   Updated: 2023/07/12 07:48:17 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*gnltest(int fd, size_t BUFFER_SIZE)
+{
+	static char *reserve;
+	char *next_line;
+	char *buffer;
+	ssize_t	read_return;
+	
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buffer[BUFFER_SIZE + 1] = '\0';
+	if (!buffer)
+		return (NULL);
+	read_return = read(fd, buffer, BUFFER_SIZE);
+	if (read_return == 0 || read_return < 0)
+		return (NULL);
+	if (reserve)
+		reserve = ft_strjoin(reserve, buffer);
+	else
+		reserve = ft_strdup(buffer);
+	while (ft_strchr(reserve, '\n'))
+	{
+		next_line = ft_substr(reserve, 0, (size_to_nl(reserve) + 1));
+		reserve = ft_strdup((ft_strchr(reserve, '\n') + 1));
+	}
+	if (reserve == NULL)
+	{
+		next_line = NULL;
+	}
+	return (next_line);
--- gnl_test.c
+++ gnl_test.c
@@ -6,7 +6,7 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 17:12:21 by pmateo            #+#    #+#             */
-/*   Updated: 2023/07/06 20:19:41 by pmateo           ###   ########.fr       */
+/*   Updated: 2023/07/12 08:43:05 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ size_t	ft_strlen(const char *str)
 
 	i = 0;
 	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	size_to_nl(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while(str[i] != '\n')
 		i++;
 	return (i);
 }
@@ -89,59 +99,122 @@ char	*ft_substr(const char *src, unsigned int start, size_t len)
 	}
 }
 
-
-// le but de cette fonction sera dans un premier temps d'effectuer des tests de manipulations pour 
-// qu'après un appel de la fonction gnltest() *str soit récupérer depuis le fichier test par open et
-// read et stockée entièrement malgré un buffer_size de 17 qui est donc sensé la lire en 3 fois.
-
-char	*gnltest(int fd, size_t BUFFER_SIZE, char *buffer)
+char	*ft_strdup(const char *s1)
 {
-	static char	*reserve;
-	char	*tmp;
-	int	newlinefound;
-	int	sizeofnl;
-	char	*tmpsave;
-	int	countcpy;
-	
-	sizeofnl = 0;
-	newlinefound = 0;
-	tmp[0] = '\0';
-	while(newlinefound != 1)
+	size_t	size;
+	char	*s2;
+
+	size = ft_strlen(s1) + 1;
+	s2 = malloc(size * sizeof(char));
+	if (s2 == NULL)
+		return (NULL);
+	ft_strlcpy(s2, s1, size);
+	return (s2);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	size_t			size;
+	char			*str;
+
+	i = 0;
+	j = 0;
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = malloc(size * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
 	{
-		read(fd, buffer, BUFFER_SIZE);
-		buffer[BUFFER_SIZE] = '\0';
-		ft_strlcpy(tmp, buffer, BUFFER_SIZE + 1);
-		countcpy++;
-		if(countcpy == 1)
-			tmpsave = tmp;
-		if(ft_strchr(buffer, '\n'))
-		{
-			tmp = ft_substr(tmpsave, 0, (ft_strchr(tmp, '\n') - tmpsave));
-			sizeofnl = ft_strlen(tmp);
-			newlinefound = 1;
-			reserve = malloc(sizeofnl * sizeof(char));
-			ft_strlcpy(reserve, tmp, sizeofnl);
-			return (reserve);
-		}
-		tmp += BUFFER_SIZE;
+		str[i] = s1[i];
+		i++;
 	}
-	return (reserve);
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+
+// void	read_and_check(char *buffer, int fd, size_t BUFFER_SIZE)
+// {
+// 	ssize_t read_return;
+
+// 	read_return = read(fd, buffer, BUFFER_SIZE);
+// 	if()
+// }
+
+char	*gnltest(int fd, size_t BUFFER_SIZE)
+{
+	static char *reserve;
+	char *next_line;
+	char *buffer;
+	ssize_t	read_return;
+	
+	if(BUFFER_SIZE == 0)
+		return(NULL);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	buffer[BUFFER_SIZE + 1] = '\0';
+	read_return = read(fd, buffer, BUFFER_SIZE);
+	if (read_return < 0)
+		return (NULL);
+	while (read_return != 0)
+	{
+		if (reserve)
+			reserve = ft_strjoin(reserve, buffer);
+		else
+			reserve = ft_strdup(buffer);
+		while (ft_strchr(reserve, '\n'))
+		{
+			next_line = ft_substr(reserve, 0, (size_to_nl(reserve) + 1));
+			reserve = ft_strdup((ft_strchr(reserve, '\n') + 1));
+			break;
+		}
+	}
+	if (!read_return)
+	{
+		while (reserve)
+		{
+			reserve = ft_strjoin(reserve, buffer);
+		}
+		
+	}
+	if (reserve == NULL)
+	{
+		next_line = NULL;
+	}
+	return (next_line);
 }
 
 int	main(void)
 {
 	// "Il etait une fois l'histoire d'un ado"; len = 37 (Il etait une fois = 17)
-	size_t BUFFER_SIZE = 17;
+	size_t BUFFER_SIZE = 100;
+	char *result;
 	int	fd = open("test.txt", O_RDONLY);
 	printf("fd = %d\nBUFFER_SIZE = %ld\n", fd, BUFFER_SIZE);
-	char *buffer;
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (!buffer)
-		exit(EXIT_FAILURE);
 	// printf("retour de read : %ld\n", read(fd, buffer, BUFFER_SIZE));
 	// gnltest(fd, BUFFER_SIZE, buffer);
-	char *result = gnltest(fd, BUFFER_SIZE, buffer);
-	printf("GNL : %s\n", result);
-	printf("mon buffer contient : '%s'\n", buffer);
-	free(buffer);
+	while(result != NULL)
+	{
+		result = gnltest(fd, BUFFER_SIZE);
+		printf("GNL : %s\n", result);
+	}
 }
+
+// int	main(void)
+// {
+// 	size_t BUFFER_SIZE = 10000;
+// 	char	*buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+// 	int fd = open("test.txt", O_RDONLY);
+// 	read(fd, buffer, BUFFER_SIZE);
+// 	buffer[BUFFER_SIZE + 1] = '\0';
+// 	printf(" ")
+// }
